3b2fb038136c14a21d52eb053fef04162080f427
Margie Fuertes <85895396+MFuertes1012@users.noreply.github.com>
2021-11-18 10:26:25 -0600

    Add files via upload

---
+++ Datos/Entidades/Producto.cs
@@ -0,0 +1,22 @@
+﻿using Datos.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datos.Entidades
+{
+    public class Producto
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public UnidadMedida UnidadMedida { get; set; }
+
+
+    }
+}
